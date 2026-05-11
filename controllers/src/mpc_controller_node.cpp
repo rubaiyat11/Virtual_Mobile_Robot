8c990ee356380e0b709099b2047d19abb0ececda
rubaiyat11 <rubaiyatabdullahrabi71@gmail.com>
2026-05-11 23:56:10 +0600

    Fixed syntax errors and completed initial Mpc node template

--- controllers/src/mpc_controller_node.cpp
+++ controllers/src/mpc_controller_node.cpp
@@ -7,7 +7,7 @@ class mpc_ctrlr_node : public rclcpp::Node{
 public:
     mpc_ctrlr_node() : Node("mpc_ctrlr_node"){
         measured_plant_state_sub = this->create_subscription<mission_interface::msg::SensorState>(
-            "/measured_state", 20, std::bind(&mpc_ctrlr_node::measured_state_callback, this, std::placeholder::_1)
+            "/measured_state", 20, std::bind(&mpc_ctrlr_node::measured_state_callback, this, std::placeholders::_1)
         );
 
         target_state_sub = this->create_subscription<mission_interface::msg::TargetState>(
@@ -17,19 +17,38 @@ public:
         mpc_output_pub = this->create_publisher<mission_interface::msg::DesiredState>(
             "/desired_state", 20
         );
+
+        mpc_pub_timer = this->create_wall_timer(
+            std::chrono::milliseconds(10),
+            std::bind(&mpc_ctrlr_node::timer_callback, this)
+        );
     }
 
 private:
+
+    void measured_state_callback(const mission_interface::msg::SensorState::SharedPtr msg){
+
+    }
+
+    void target_state_callbaack(const mission_interface::msg::TargetState::SharedPtr msg){
+
+    }
+
+    void timer_callback(){
+
+    }
+
     rclcpp::Subscription<mission_interface::msg::SensorState>::SharedPtr measured_plant_state_sub;
     rclcpp::Subscription<mission_interface::msg::TargetState>::SharedPtr target_state_sub;
     rclcpp::Publisher<mission_interface::msg::DesiredState>::SharedPtr mpc_output_pub;  
+    const mission_interface::msg::SensorState::SharedPtr msg;
 };
 
 
 
 int main(int argc, char **argv){
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<plant_node>();
+    auto node = std::make_shared<mpc_ctrlr_node>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
