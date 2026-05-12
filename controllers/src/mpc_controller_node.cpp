dd655285c30e43c05ffe18fcd9f78dd994d1f352
rubaiyat11 <rubaiyatabdullahrabi71@gmail.com>
2026-05-12 21:55:08 +0600

    Updated MPC node

--- controllers/src/mpc_controller_node.cpp
+++ controllers/src/mpc_controller_node.cpp
@@ -2,6 +2,7 @@
 #include "mission_interface/msg/sensor_state.hpp"
 #include "mission_interface/msg/target_state.hpp"
 #include "mission_interface/msg/desired_state.hpp"
+#include <Eigen/Dense>
 
 class mpc_ctrlr_node : public rclcpp::Node{
 public:
@@ -28,9 +29,29 @@ private:
 
     void measured_state_callback(const mission_interface::msg::SensorState::SharedPtr msg){
 
+        x(0) = msg->measured_position[0];
+        x(1) = msg->measured_position[1];
+        x(2) = msg->measured_position[2];
+
+        x(3) = msg->measured_velocity[0];
+        x(4) = msg->measured_velocity[1];
+        x(5) = msg->measured_velocity[2];
+
     }
 
-    void target_state_callbaack(const mission_interface::msg::TargetState::SharedPtr msg){
+    void target_state_callback(const mission_interface::msg::TargetState::SharedPtr msg){
+        
+        x_target(0) = msg->target_position[0];
+        x_target(1) = msg->target_position[1];
+        x_target(2) = msg->target_position[2];
+
+        x_target(3) = msg->target_velocity[0];
+        x_target(4) = msg->target_velocity[1];
+        x_target(5) = msg->target_velocity[2];
+
+    }
+
+    void compute_mpc(){
 
     }
 
@@ -38,10 +59,23 @@ private:
 
     }
 
+
+    double dt = 0.01;
+
+    Eigen::Matrix<double, 6, 1> x;
+    Eigen::Matrix<double, 6, 1> x_target;
+    Eigen::Vector3d u;
+
+    Eigen::Matrix<double, 6, 6> A;
+    Eigen::Matrix<double, 6, 3> B;
+
+    Eigen::Matrix<double, 6, 6> Q;
+    Eigen::Matrix<double, 3, 3> R;
+
     rclcpp::Subscription<mission_interface::msg::SensorState>::SharedPtr measured_plant_state_sub;
     rclcpp::Subscription<mission_interface::msg::TargetState>::SharedPtr target_state_sub;
     rclcpp::Publisher<mission_interface::msg::DesiredState>::SharedPtr mpc_output_pub;  
-    const mission_interface::msg::SensorState::SharedPtr msg;
+    rclcpp::TimerBase::SharedPtr mpc_pub_timer;
 };
 
 
