d48294276510a5d94bd7fc1ea4f574bf103864c3
rubaiyat11 <rubaiyatabdullahrabi71@gmail.com>
2026-05-18 23:00:44 +0600

    Fixed few LQR Node bugs

--- controllers/src/lqr_controller_node.cpp
+++ controllers/src/lqr_controller_node.cpp
@@ -1,12 +1,12 @@
 #include "rclcpp/rclcpp.hpp"
 #include "mission_interface/msg/desired_state.hpp"
-#include "mission_interface/msg/adjusted_state.hpps"
+#include "mission_interface/msg/adjusted_state.hpp"
 
 class lqr_controller_node : public rclcpp::Node{
 public:
     lqr_controller_node() : Node("lqr_controller_node"){
         desired_state_sub = this->create_subscription<mission_interface::msg::DesiredState>(
-            "/desired_state", 20, std::bind(&lqr_controller_node::adjusted_state_callback, this, std::placeholders::_1)
+            "/desired_state", 20, std::bind(&lqr_controller_node::desired_state_callback, this, std::placeholders::_1)
         );
 
         adjusted_state_pub = this->create_publisher<mission_interface::msg::AdjustedState>(
@@ -20,9 +20,9 @@ private:
 };
 
 int main(int argc, char **argv){
-    rclcpp::inti(argc, argv);
+    rclcpp::init(argc, argv);
     auto node = std::make_shared<lqr_controller_node>();
     rclcpp::spin(node);
-    rclcpp::shutdown;
+    rclcpp::shutdown();
     return 0;
 }
